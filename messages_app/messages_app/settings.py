26fdb71345d6ce0e7a231c6ca13e848c0325fc26
amiellib <amiel230@walla.com>
2022-11-22 14:56:11 +0200

    heroku

--- messages_app/messages_app/settings.py
+++ messages_app/messages_app/settings.py
@@ -146,3 +146,7 @@ AUTH_USER_MODEL = 'user_api.UsersProfile'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_heroku
+django_heroku.settings(locals())
